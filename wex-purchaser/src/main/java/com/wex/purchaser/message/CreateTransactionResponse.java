c6de4e379f1f9057fd8171cf92c0c84f06abcc92
Ray Cheng <ray101682@gmail.com>
2024-04-29 14:27:02 +0100

    first comment all 3 projects

---
+++ wex-purchaser/src/main/java/com/wex/purchaser/message/CreateTransactionResponse.java
@@ -0,0 +1,15 @@
+package com.wex.purchaser.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateTransactionResponse extends AbstractResponse{
+	
+	private int result;
+}
